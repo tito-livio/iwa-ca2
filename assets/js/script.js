8979acc9656d7aa535846f90982b83437ccc6c6c
Ahsan <55291948+tito-livio@users.noreply.github.com>
2022-04-27 14:51:15 +0500

    Added Highlight styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,52 @@
-$("#add_car").submit(function() {
-    alert("Data inserted successfully");
-})
+$("#add_car").submit(function () {
+  alert("Data inserted successfully");
+});
 
-$("#update_car").submit(function(event) {
-    event.preventDefault();
-    let unindexed_array = $(this).serializeArray();
-    let data = {};
-    $.map(unindexed_array, function(n, i) {
-        data[n['name']] = n['value'];
-    });
-    let request = {
-        "url": `api/update/${data.id}`,
-        "method": "PATCH",
-        "data": data
-    }
-    console.log(request);
-    $.ajax(request).done((response) => {
-        alert("Data Upadated Successfully");
-    })
-})
+$("#update_car").submit(function (event) {
+  event.preventDefault();
+  let unindexed_array = $(this).serializeArray();
+  let data = {};
+  $.map(unindexed_array, function (n, i) {
+    data[n["name"]] = n["value"];
+  });
+  let request = {
+    url: `api/update/${data.id}`,
+    method: "PATCH",
+    data: data,
+  };
+  console.log(request);
+  $.ajax(request).done((response) => {
+    alert("Data Upadated Successfully");
+  });
+});
 
 if (window.location.pathname == "/edit-car") {
-    $ondelete = $(".table tbody td a.delete");
-    $ondelete.click(function() {
-        var id = $(this).attr("data-id");
-        var request = {
-            "url": `http://localhost:4000/api/delete/${id}`,
-            "method": "DELETE"
-        }
-        if (confirm("Are you sure you want to delete this record?")) {
-            $.ajax(request).done((response) => {
-                alert("Data Deleted Successfully");
-                location.reload();
-            })
+  $ondelete = $(".table tbody td a.delete");
+  $ondelete.click(function () {
+    var id = $(this).attr("data-id");
+    var request = {
+      url: `http://localhost:4000/api/delete/${id}`,
+      method: "DELETE",
+    };
+    if (confirm("Are you sure you want to delete this record?")) {
+      $.ajax(request).done((response) => {
+        alert("Data Deleted Successfully");
+        location.reload();
+      });
+    }
+  });
+}
+//Function to select any row via the checkbox of that row
+//each checkbox has a number assigned to it -> matches the number of the table row
+//and highlights it by adding a class and selectes it
 
-        }
-    })
+function selectRow(e, n) {
+  //If the row is selected then remove the selected class
+  //otherwise add it
+  let checked = e.target.checked;
+  if (checked == false) {
+    $(`#carTableRow${n}`).removeClass("selected");
+  } else {
+    $(`#carTableRow${n}`).addClass("selected");
+  }
 }
