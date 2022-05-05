ba5e0e0ba6e250ef2f5c571e8591bbb0be15cd48
Tito Livio <baiev@msn.com>
2022-05-05 02:56:24 +0100

    organizing&comments

--- server/middlewares/sanitization.js
+++ server/middlewares/sanitization.js
@@ -1,24 +1,30 @@
-import { body } from "express-validator";
+/**
+ * @author: Tito Livio - 2018253
+ * @description: This file is responsible for sanitization of the client input
+ */
 
+//import body from express-validator;
+import { body } from "express-validator";
+//sanitation item by item which is necessary for the validation
 export const sanitize_name = body("name")
-  .not()
-  .isEmpty()
-  .isString()
-  .trim()
-  .escape();
+    .not()
+    .isEmpty()
+    .isString()
+    .trim()
+    .escape();
 
 export const sanitize_fuel = body("fuel")
-  .not()
-  .isEmpty()
-  .isString()
-  .trim()
-  .escape();
+    .not()
+    .isEmpty()
+    .isString()
+    .trim()
+    .escape();
 
 export const sanitize_type = body("type")
-  .not()
-  .isEmpty()
-  .isString()
-  .trim()
-  .escape();
+    .not()
+    .isEmpty()
+    .isString()
+    .trim()
+    .escape();
 
 export const sanitize_price = body("price").isNumeric();
--- server/middlewares/validation.js
+++ server/middlewares/validation.js
@@ -1,22 +1,30 @@
+/**
+ * @author: Tito Livio - 2018253
+ * @description: This file is responsible for validations of the client input
+ */
+
+//import check from "express-validator";
 import { check } from "express-validator";
 
+//calling the instance of the validator function
 export const validate = [
-  check("name", "name must be greater than 5 characters")
+    //checking item by item which is necessary for the validation
+    check("name", "name must be greater than 5 characters")
     .exists()
     .isAlphanumeric()
     .isLength({
-      min: 5,
+        min: 5,
     }),
-  check("type", "type must be greater than 5 characters")
+    check("type", "type must be greater than 5 characters")
     .exists()
     .isAlphanumeric()
     .isLength({
-      min: 5,
+        min: 5,
     }),
-  check("fuel", "fuel must be greater than 5 characters")
+    check("fuel", "fuel must be greater than 5 characters")
     .exists()
     .isAlphanumeric()
     .isLength({
-      min: 5,
+        min: 5,
     }),
 ];
